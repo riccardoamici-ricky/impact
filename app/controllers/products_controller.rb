5890fc51ecf792ec6148b6c8c38908ca76b1ce48
Peter Hemsing <peterhemsing@gmail.com>
2020-11-25 18:10:25 +0100

    show of product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  def index
-    @products = Product.all
+  def show
+    @product = Product.find(params[:id])
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,21 +2,9 @@
 require 'open-uri'
 require 'nokogiri'
 
-url = "https://bosqueplants.com/en/plants"
-html_file = open(url).read
-html_doc = Nokogiri::HTML(html_file)
-html_doc.search(".product").each_with_index do |box, index|
-  title =  box.search('h5').text
-  desc =  box.search('p').text
-  price =  box.search('.price').text.strip
-  img = box.search('img').attribute('src').value
-  Product.create(title: title, description: desc, price: price, image: img)
-  puts "Created product #{index + 1}"
-end
-  puts "Finish creating products"
-
+Product.delete_all
 Event.destroy_all
-User.destroy_all
+# User.destroy_all
 Category.destroy_all
 
 puts 'Creating categories'
@@ -26,8 +14,34 @@ Category.create(name: 'Food')
 Category.create(name: 'Interior')
 puts 'Habemus categories'
 
-puts 'creating user'
+url = "https://bosqueplants.com/en/plants"
+html_file = open(url).read
+html_doc = Nokogiri::HTML(html_file)
+html_doc.search(".product").each_with_index do |box, index|
+  title =  box.search('h5').text
+  url_title = title.downcase.gsub(" ", "")
+  url = "https://bosqueplants.com/en/plants/#{url_title}"
+  # p url_title
+  html_file = open(url).read
+  html_doc = Nokogiri::HTML(html_file)
+  five_tr = html_doc.search("tr:nth-child(5)")
+  six_tr = html_doc.search("tr:nth-child(6)")
+  description = five_tr.search("td:nth-child(2)").text
+  description_watering = six_tr.search("td:nth-child(2)").text
+  # p description.text
+  # p description_watering.text
+  desc =  "#{box.search('p').text} #{description} #{description_watering}"
+  price =  box.search('.price').text.strip
+  img = box.search('img').attribute('src').value
+  Product.create!(title: title, description: desc, price: price, image: img, category: Category.all.sample)
+  puts "Created product #{index + 1}"
+end
+puts "Congrats you now have #{Product.count} products."
+puts "Finish creating products"
 
+
+
+puts 'creating user'
 laura = User.create(email: 'laura@gmail.com', password: '123456')
 
 puts 'creating events'
